1ab1e0826e6af4b748c1b638d66e15ff3e67aa6f
Sagnik Pramanik <sagnikpramanik95@gmail.com>
2024-12-31 21:20:01 +0530

    Update Create_Collection.py

--- Create_Collection.py
+++ Create_Collection.py
@@ -1,7 +1,7 @@
 import os
 from astrapy import DataAPIClient
 from dotenv import load_dotenv
-
+import csv
 
 load_dotenv()
 ASTRA_TOKEN = os.getenv("ASTRA_TOKEN")
@@ -12,5 +12,6 @@ db = client.get_database_by_api_endpoint(ASTRA_DB_URL)
 COLLECTION_NAME = os.getenv("COLLECTION_NAME")
 if COLLECTION_NAME not in db.list_collection_names():
     db.create_collection(COLLECTION_NAME)
-
-print(f"Connected to Astra DB, collections are: {db.list_collection_names()}")
+    print("Created Collection")
+else:
+    print("Collection already exists")
